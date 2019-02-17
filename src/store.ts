935fa4463dc8b65fdae4eda79b099be5f9ae9990
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-02-18 00:02:35 +1300

    Add model state

--- src/store.ts
+++ src/store.ts
@@ -5,10 +5,28 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    url: '',
+    title: '',
+    author: '',
+  },
+  getters: {
+    html() {
+      return '<html...>';
+    },
+    git() {
+      return 'Add ABC\'s blablabla';
+    },
   },
   mutations: {
-
+    setUrl(state, url) {
+      state.url = url;
+    },
+    setTitle(state, title) {
+      state.title = title;
+    },
+    setAuthor(state, author) {
+      state.author = author;
+    },
   },
   actions: {
 
