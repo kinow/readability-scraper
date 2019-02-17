7680cd6cb2c9c4d810a11e173231a51c535d46f1
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-02-17 23:54:42 +1300

    Add initial components in the UI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        <div class="card">
+          <div class="card-body">
+            <h2 class="card-title">Readability Scraper</h2>
+            <form>
+              <div class="form-group">
+                <label for="url">URL</label>
+                <input type="url" class="form-control form-control-lg" id="url" aria-describedby="urlHelp" placeholder="Enter URL">
+                <small id="urlHelp" class="form-text text-muted">We'll use mozilla/readability to parse and extract title and author.</small>
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm">
+        <div class="card">
+          <div class="card-body">
+            <div class="form-group">
+              <label for="html">HTML</label>
+              <textarea class="form-control" id="html" rows="3">ABCD!</textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm">
+        <div class="card">
+          <div class="card-body">
+            <div class="form-group">
+              <label for="html">Git commit message</label>
+              <textarea class="form-control" id="git" rows="3">ABCD!</textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
