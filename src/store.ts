6c4d798695337158902b6b998dac19894376190f
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-02-18 16:36:06 +1300

    Add alerts whenever an error happens

--- src/store.ts
+++ src/store.ts
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from 'axios';
 
+import { Alert, AlertType } from '@/utils';
+
 import {JSDOM} from 'jsdom';
 /// <reference path="../node_modules/@types/mozilla-readability/index.d.ts"/>
 import Readability from 'readability';
@@ -14,12 +16,16 @@ export default new Vuex.Store({
     title: '',
     author: '',
     isLoading: false,
+    alerts: [],
   },
   getters: {
     url: (state) => {
       return state.url;
     },
     html: (state) => {
+      if (!state.title || ! state.author) {
+        return;
+      }
       if (state.title.trim() === '' || state.author.trim() === '') {
         return '';
       }
@@ -29,10 +35,16 @@ export default new Vuex.Store({
         `</tr>`;
     },
     git: (state) => {
+      if (!state.title || ! state.author) {
+        return;
+      }
       if (state.title.trim() === '' || state.author.trim() === '') {
         return '';
       }
       return `Add ${state.author}'s ${state.title}`;
+    },
+    alerts: (state) => {
+      return state.alerts;
     },
   },
   mutations: {
@@ -48,10 +60,24 @@ export default new Vuex.Store({
     setLoading(state, isLoading) {
       state.isLoading = isLoading;
     },
+    addAlert(state, alert) {
+      // @ts-ignore
+      state.alerts.push(alert);
+    },
+    clearAlerts(state) {
+      state.alerts = [];
+    },
   },
   actions: {
     read({commit}) {
+      commit('clearAlerts');
+      commit('setTitle', '');
+      commit('setAuthor', '');
       if (this.state.url.trim() === '') {
+        const alert = new Alert();
+        alert.detail = 'Missing URL';
+        alert.type = AlertType.WARNING;
+        commit('addAlert', alert);
         return;
       }
       commit('setLoading', true);
@@ -59,7 +85,7 @@ export default new Vuex.Store({
       const origin = window.location.protocol + '//' + window.location.host;
       axios(url, {
         maxRedirects: 0,
-        timeout: 3600,
+        timeout: 9000,
         headers: {
           'Accept': '*/*',
           'Cache-Control': 'no-cache',
@@ -78,10 +104,25 @@ export default new Vuex.Store({
         .catch((error) => {
           if (error.response) {
             // console.log(error.response.headers);
+            const alert = new Alert();
+            alert.title = 'Server Error';
+            alert.detail = error.message;
+            alert.type = AlertType.DANGER;
+            commit('addAlert', alert);
           } else if (error.request) {
             // console.log(error.request);
+            const alert = new Alert();
+            alert.title = 'Request Error';
+            alert.detail = error.message;
+            alert.type = AlertType.DANGER;
+            commit('addAlert', alert);
           } else {
             // console.log(error.message);
+            const alert = new Alert();
+            alert.title = 'Unknown Error!';
+            alert.detail = error.message;
+            alert.type = AlertType.DANGER;
+            commit('addAlert', alert);
           }
           // console.log(error.config);
           commit('setLoading', false);
